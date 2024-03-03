d2c90ac673691ca70969f72484606823b99be729
Connor Pilkey <97925854+kensho-pilkey@users.noreply.github.com>
2024-03-02 21:50:39 -0500

    Delete Scripts directory

--- Scripts/Resource.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name ItemResource
-
-# Define the properties of your resource
-@export var item_id: String
-@export var name: String
-@export var description: String
-@export var quantity: int = 0
-@export var icon: Texture
